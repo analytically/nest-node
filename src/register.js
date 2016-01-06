006a9d8fdc72afdba3ceaf3620d0feacee1f7754
Mathias Bogaert <mathias.bogaert@gmail.com>
2016-01-06 18:02:54 +0000

    Fix register link.

--- src/register.js
+++ src/register.js
@@ -2,11 +2,11 @@
 export default function register(callback) {
     let app = process.env.NEST_CLIENT_ID
     if (typeof app === 'undefined') {
-        callback(Error('missing process.env.nest_CLIENT_ID'))
+        callback(Error('missing process.env.NEST_CLIENT_ID'))
     }
     else {
         let url = 'https://home.nest.com/login/oauth2'
-        let query = `?client_id=${app}&state=FOO`
+        let query = `?client_id=${app}`
         callback(null, `${url}${query}`)
     }
 }
